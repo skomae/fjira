7b9c7db328c9d4e32a0a494bbd0acb6a742280b4
Stephen Komae <stephen_komae@intuit.com>
2025-07-02 12:02:40 -0400

    add --board= arg

--- internal/boards/board_view.go
+++ internal/boards/board_view.go
@@ -63,6 +63,10 @@ type boardView struct {
 }
 
 func NewBoardView(project *jira.Project, boardConfiguration *jira.BoardConfiguration, filterJQL string, api jira.Api) app.View {
+	if boardConfiguration != nil {
+		app.SetLastViewedBoardID(boardConfiguration.Id)
+	}
+
 	col := 0
 	statusesColumnsMap := map[string]int{}
 	columnStatusesMap := map[int][]string{}
